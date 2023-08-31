b9a6d75202cc779d45b8af027b02f0e300f0269d
Obi <williamobi818@gmail.com>
2023-08-31 22:21:48 +0000

    udo

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -7,16 +7,13 @@
  */
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-	int left_nodes;
-	int right_nodes;
+	int left_nodes = 0;
+	int right_nodes = 0;
 
 	if (tree == NULL)
 	{
 		return (0);
 	}
-
-	left_nodes = 0;
-	right_nodes = 0;
 
 	left_nodes = tree_height(tree->left);
 	right_nodes = tree_height(tree->right);
@@ -29,4 +26,27 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 	{
 		return (0);
 	}
+}
+/**
+ * tree_height - tree_height
+ * @tree: tree to chk
+ * Return: height
+ */
+int tree_height(const binary_tree_t *tree)
+{
+	int left_height;
+	int right_height;
+
+	if (tree == NULL)
+	{
+		return (0);
+	}
+
+	left_height = 0;
+	right_height = 0;
+
+	left_height = tree->left ? 1 + tree_height(tree->left) : 1;
+	right_height = tree->right ? 1 + tree_height(tree->right) : 1;
+
+	return ((left_height > right_height) ? left_height : right_height);
 }
